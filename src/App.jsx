458ff7e5d979be4c0968ac4e78d9811309a7503e
neo <leon.mwashi@gmail.com>
2026-08-16 00:32:22 +0300

    feat: set up routing for product and shop pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+<<<<<<< HEAD
+import { BrowserRouter, Routes, Route } from 'react-router'
+import ProductPage from './products/ProductPage'
+import ProductList from './products/ProductList'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/shop" element={<ProductList />} />
+        <Route path="/" element= {
+          <LandingPage />
+        } />
+      </Routes>
+    </BrowserRouter>
   )
+
 }
 
 export default App;
